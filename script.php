b78ddd784c7091b7ab3ef0bddd35e164c0c234bc
Aitor Piris <aitorpiris@opendeusto.es>
2022-08-14 09:16:44 +0200

    ajuste de url

--- script.php
+++ script.php
@@ -97,14 +97,14 @@
       $name = 'usuario';
       $value = $usuario;
       $path = '/';
-      $domain = 'http://accessia.click/';
+      $domain = 'accessia.click';
       setcookie($name , $value , time() + (86400 * 30) , $path , $domain );
 
       $usuario = utf8_decode($idUsuario);
       $id = 'idUsuario';
       $value = $idUsuario;
       $path = '/';
-      $domain = 'http://accessia.click/';
+      $domain = 'accessia.click';
       setcookie($id , $value , time() + (86400 * 30) , $path , $domain );
 
       echo $_COOKIE[$name].' | '.$_COOKIE[$id];
