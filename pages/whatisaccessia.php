5f9337340c54162d9fe08f3b1c8b543dc386368d
Aitor Piris <aitorpiris@opendeusto.es>
2022-09-01 01:38:31 +0200

    no message

---
+++ pages/whatisaccessia.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+		<meta charset="utf-8"/>
+		<title>ACCESSIA TFG</title>
+		<link rel="stylesheet" type="text/css" href="../styles/estilos.css">
+		<link rel="stylesheet" type="text/css" href="../styles/footerStyle.css">
+        <link rel="stylesheet" type="text/css" href="../styles/usuario.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="../js/loadFunctions.js"></script>
+        <script src="../js/usuario.js"></script>
+		<link href='https://fonts.googleapis.com/css?family=Cormorant Garamond' rel='stylesheet'>
+        <script src="../js/chart/dist/chart.min.js"></script>
+	</head>
+    <body>
+            <div class="accessia">
+                <h1>¿Que es Accessia?</h1>
+            </div>
+    </body>
+    
+</html>
