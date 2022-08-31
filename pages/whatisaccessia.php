aec78f90af580350f4ba1f1a82f8a8aa05a3fd42
Aitor Piris <aitorpiris@opendeusto.es>
2022-09-01 01:39:36 +0200

    no message

--- pages/whatisaccessia.php
+++ pages/whatisaccessia.php
@@ -1,20 +1,38 @@
 <html>
-<head>
+    <head>
 		<meta charset="utf-8"/>
-		<title>ACCESSIA TFG</title>
+		<title>Trabajo Fin De Grado</title>
 		<link rel="stylesheet" type="text/css" href="../styles/estilos.css">
 		<link rel="stylesheet" type="text/css" href="../styles/footerStyle.css">
-        <link rel="stylesheet" type="text/css" href="../styles/usuario.css">
+		<link rel="stylesheet" type="text/css" href="../styles/registroUsuario.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<script src="../js/loadFunctions.js"></script>
-        <script src="../js/usuario.js"></script>
+		<script src="/js/loadFunctions.js"></script>
+		<script src="/js/registro.js"></script>
 		<link href='https://fonts.googleapis.com/css?family=Cormorant Garamond' rel='stylesheet'>
-        <script src="../js/chart/dist/chart.min.js"></script>
 	</head>
+
     <body>
-            <div class="accessia">
+        <div id="cabecera">
+			<script type="text/javascript">
+				loader = new Loader();
+				loader.loadHeader();
+			</script>
+		</div>
+
+
+        <div class="accessia">
                 <h1>¿Que es Accessia?</h1>
             </div>
+
     </body>
-    
+
+
+    <footer>
+		<div id="footer">
+			<script type="text/javascript">
+				loader = new Loader();
+				loader.loadFooter();
+			</script>
+		</div>
+	</footer>
 </html>
